dfcd574a0eb676ef21017ad90488e039cb20aff6
andrew4alive <andrew.tanwahseong@hotmail.com>
2016-10-05 21:09:45 +0800

    first commit
    create and mark your location

---
+++ wgmap.js
@@ -0,0 +1,52 @@
+//gmap lib 
+    function wgmap (apikey){
+      var self=this;
+      self.umoption=null;
+      self.umark=null;
+      window.onload=function(){
+        
+        
+        document.body.appendChild(mapele(apikey));
+       
+        self.map = function(ele,option,ub){//create google map
+            var map = new google.maps.Map(ele, option);
+            var my ;
+          if(ub==true){
+           var my = umarkf(map);
+          }
+             self.umark = my;
+            return map;       
+        };
+        
+      //  self.umark=function(){ return umarkf(); };//create your mark
+      };
+      
+      function umarkf(map){
+        
+        var my;
+      if(self.umoption !== null){
+             my = new google.maps.Marker(self.umoption);
+             }else{
+             my = new google.maps.Marker({
+              position: map.getCenter(),
+              icon: {
+                  path: google.maps.SymbolPath.CIRCLE,
+                  scale: 5,
+                 strokeColor: 'blue'},
+               draggable: false,
+               map: map});
+            }
+          return my;
+      }
+      
+      function mapele(ak){// create gmap api
+      var scriptEle = document.createElement('script');
+      scriptEle.setAttribute('async','');
+      scriptEle.setAttribute('defer','');
+      scriptEle.setAttribute('src','https://maps.googleapis.com/maps/api/js?key='+ak+'&callback=initMap&libraries=places,visualization');
+      return scriptEle; }
+      
+      
+        
+      
+    };//end wgmap
